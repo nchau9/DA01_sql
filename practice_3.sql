de8921fa8095e1de57676983dea51231b339289a
nchau9 <117317567+nchau9@users.noreply.github.com>
2024-06-04 22:40:01 -0700

    Update practice_3.sql

--- practice_3.sql
+++ practice_3.sql
@@ -22,3 +22,24 @@ ROUND(AVG(stars),2) AS avg_stars
 FROM reviews
 GROUP BY mth, product_id
 ORDER BY mth, product_id
+--ex5 https://datalemur.com/questions/teams-power-users
+SELECT sender_id,
+COUNT(message_id) AS message_count
+FROM messages
+WHERE EXTRACT(MONTH FROM sent_date)=8
+AND EXTRACT(YEAR FROM sent_date)=2022
+GROUP BY sender_id
+ORDER BY message_count DESC
+LIMIT 2
+--ex6 https://leetcode.com/problems/invalid-tweets/description/?envType=study-plan-v2&envId=top-sql-50
+SELECT tweet_id
+FROM tweets
+WHERE LENGTH(content)>15
+--ex7 https://leetcode.com/problems/user-activity-for-the-past-30-days-i/description/?envType=study-plan-v2&envId=top-sql-50
+--ex8 https://platform.stratascratch.com/coding/2151-number-of-hires-during-specific-time-period?code_type=1
+SELECT  
+COUNT(employee_id) as number_employee
+from employees
+WHERE EXTRACT (MONTH FROM joining_date) between 1 and 7
+AND EXTRACT (YEAR FROM joining_date)=2022
+--ex9 https://platform.stratascratch.com/coding/9829-positions-of-letter-a?code_type=1
