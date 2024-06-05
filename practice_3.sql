6d54a32ebfbfa3085feed71950e19c7767bd190c
nchau9 <117317567+nchau9@users.noreply.github.com>
2024-06-04 22:50:42 -0700

    Update practice_3.sql

--- practice_3.sql
+++ practice_3.sql
@@ -36,6 +36,12 @@ SELECT tweet_id
 FROM tweets
 WHERE LENGTH(content)>15
 --ex7 https://leetcode.com/problems/user-activity-for-the-past-30-days-i/description/?envType=study-plan-v2&envId=top-sql-50
+SELECT activity_date,
+COUNT(DISTINCT user_id) AS active_users
+FROM activity
+WHERE activity_date BETWEEN DATE '2019-06-28' AND DATE '2019-07-27'
+GROUP BY activity_date
+ORDER BY activity_date
 --ex8 https://platform.stratascratch.com/coding/2151-number-of-hires-during-specific-time-period?code_type=1
 SELECT  
 COUNT(employee_id) as number_employee
