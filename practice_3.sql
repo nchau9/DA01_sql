2ae52f09ef88e6de2f8738f948bf4c0222ab9c83
nchau9 <117317567+nchau9@users.noreply.github.com>
2024-06-04 20:40:31 -0700

    Update practice_3.sql

--- practice_3.sql
+++ practice_3.sql
@@ -9,3 +9,16 @@ CONCAT (UPPER(LEFT(name,1)), LOWER (RIGHT(name, LENGTH(name)-1))) AS name
 FROM Users
 ORDER BY user_id
 --ex3 https://datalemur.com/questions/total-drugs-sales
+SELECT manufacturer,
+'$'||ROUND(sum(total_sales)/1000000,0)||' '||'million'AS sale
+FROM pharmacy_sales
+GROUP BY manufacturer
+ORDER BY sum(total_sales) desc, manufacturer
+--ex4 https://datalemur.com/questions/sql-avg-review-ratings
+SELECT 
+EXTRACT(MONTH FROM submit_date) AS mth,
+product_id,
+ROUND(AVG(stars),2) AS avg_stars
+FROM reviews
+GROUP BY mth, product_id
+ORDER BY mth, product_id
