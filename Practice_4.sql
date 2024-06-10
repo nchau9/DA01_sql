6e84a74ca39c4b131e4a7e8f0c3bc750e78b68eb
nchau9 <117317567+nchau9@users.noreply.github.com>
2024-06-09 23:22:19 -0700

    Update Practice_4.sql

--- Practice_4.sql
+++ Practice_4.sql
@@ -14,3 +14,21 @@ ELSE 'No'
 END AS triangle
 FROM Triangle;
 --hw3 
+SELECT ROUND((COUNT(CASE WHEN call_category IS NULL OR call_category = 'n/a' THEN 1 END) / COUNT(*)) * 100, 1) AS uncategorised_call_pct
+FROM callers;
+--hw4
+SELECT c.name
+FROM Customer c
+LEFT JOIN Customer r ON c.referee_id = r.id
+WHERE r.id != 2 OR c.referee_id IS NULL;
+--hw5
+SELECT
+CASE
+WHEN pclass = 1 THEN 'first_class'
+WHEN pclass = 2 THEN 'second_class'
+WHEN pclass = 3 THEN 'third_class'
+END AS passenger_class,
+SUM(CASE WHEN survived = 1 THEN 1 ELSE 0 END) AS survivors,
+SUM(CASE WHEN survived = 0 THEN 1 ELSE 0 END) AS non_survivors
+FROM titanic
+GROUP BY passenger_class;
